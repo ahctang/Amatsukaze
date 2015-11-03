efde7ef383b8d93aeab6215b20434a544616333c
David Yim <contact.yimdavid@gmail.com>
2015-11-03 19:47:34 +0900

    First select dialog

--- Amatsukaze/Folder Menu/FolderMenuView.xaml.cs
+++ Amatsukaze/Folder Menu/FolderMenuView.xaml.cs
@@ -12,17 +12,36 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Amatsukaze.ViewModel;
+using System.Windows.Forms;
 
 namespace Amatsukaze.View
 {
     /// <summary>
     /// Interaction logic for FolderMenuView.xaml
     /// </summary>
-    public partial class FolderMenuView : UserControl
+    public partial class FolderMenuView : System.Windows.Controls.UserControl
     {
         public FolderMenuView()
         {
             InitializeComponent();
         }
+
+        private void lala(object sender, RoutedEventArgs e)
+        {
+            //var obj = this.DataContext as FolderMenuViewModel;
+            //obj.Test = "Haha";
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.ShowDialog();
+            
+            var obj = this.DataContext as FolderMenuViewModel;
+            obj.Test = dialog.SelectedPath;
+        }
+
+        //private void lala(object sender, RoutedEventArgs e)
+        //{
+        //var obj = this.DataContext as FolderMenuViewModel;
+        //obj.Test = "Haha";
+        //}
     }
 }
--- Amatsukaze/Folder Menu/FolderMenuViewModel.cs
+++ Amatsukaze/Folder Menu/FolderMenuViewModel.cs
@@ -4,17 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Amatsukaze.HelperClasses;
+using System.Windows.Forms;
 
 namespace Amatsukaze.ViewModel
 {
     class FolderMenuViewModel : ObservableObjectClass, ViewModelBase
     {
+        private string test;
+
+        public string Test {
+            get
+            {
+                if (test == null)
+                    test = "blop";
+                return test;
+            }
+            set
+            {
+                if (test != value)
+                {
+                    test = value;
+                    OnPropertyChanged("Test");
+                }
+            }
+        }
+
         public string BaseName
         {
             get
             {
                 return "Folder Menu";
             }
+            /*set
+            {
+                if (BaseName != value)
+                BaseName = value;
+                OnPropertyChanged(BaseName);
+            }*/
         }
     }
 }
